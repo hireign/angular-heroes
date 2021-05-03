66bd9e37a3cfa77f37a4f2ecb6596b90577236eb
Hirenkumar Khant <hirenkumar.khant@ibm.com>
2021-05-03 11:52:15 -0300

    imported heroes array and displayed in the heroes template using ngfor directive

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { fromEventPattern } from 'rxjs';
+// import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +10,11 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // }
 
   constructor() { }
 
