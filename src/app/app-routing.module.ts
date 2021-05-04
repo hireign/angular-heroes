e6da08c50122d3eb7772ecb53d97f9681fb8e547
Hirenkumar Khant <hirenkumar.khant@ibm.com>
2021-05-04 12:36:39 -0300

    appRoutingModule: defined routes:Routes, configured RouterModule with routes, exported RouterModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    
-  ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
