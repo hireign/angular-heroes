e190ce9dd2781b053a60f156378454ddf56a8d0b
Hirenkumar Khant <hirenkumar.khant@ibm.com>
2021-05-05 13:55:36 -0300

    updated getHeroes with http.get(), added log method Imported HttpClient In getHeroes() changed of() to http.get(), updated messageService.add to a private log method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,24 +3,33 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
 
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
+    const heroes = this.http.get<Hero[]>(this.heroesUrl);
+    this.log('fetched heroes');
     return heroes;
   }
 
   getHero(id: Number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id) as Hero;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(hero);
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string){
+    this.messageService.add(`HeroService: ${message}`);
   }
   
 }
