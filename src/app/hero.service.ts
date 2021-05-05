a5a3259f16d94eaff6484d99e83e152782962c6f
Hirenkumar Khant <hirenkumar.khant@ibm.com>
2021-05-05 09:47:21 -0300

    Added css for HeroDetailComponent also, amended a few other files

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,8 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
@@ -20,6 +22,5 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
-
-  constructor(private messageService: MessageService) { }
+  
 }
